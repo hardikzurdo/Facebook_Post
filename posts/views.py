1b358687daeb5c5f394ad572c065dbc2ed72b074
Hardikpatel11 <hardikzurdo@gmail.com>
2016-09-24 02:21:00 -0700

    Pagination from Django Documents

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 from .forms import PostForm
@@ -28,14 +29,27 @@ def post_detail(request,id=None):
 	return render(request,"post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all().order_by("-timestamp","-updated")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
 	context = {
 		"object_list":queryset,
-		"title": "List"
+		"title": "List",
+		"page_request_var" : page_request_var,
 
-	}
+		}
 	return render(request,"post_list.html", context)
 	#return HttpResponse("<h1>List</h1>")
+ 
 
 def post_update(request,id = None):
 	instance = get_object_or_404(Post,id = id)
@@ -57,3 +71,7 @@ def post_delete(request,id=None):
 	instance.delete()
 	messages.success(request, "successfully removed !!!")
 	return redirect("posts:list")	
+
+
+
+
