ef1044e59aeee4819c645575e1e1e167482d5062
Hardikpatel11 <hardikzurdo@gmail.com>
2016-09-24 22:35:21 -0700

    facebook comment and like

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,11 @@
 from __future__ import unicode_literals
+from django.conf import settings
 from django.db import models
 from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Post(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, default = 1 )
 	title = models.CharField(max_length=120)
 	image = models.FileField(null =True, blank = True)
 	content = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from urllib import quote_plus
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
@@ -9,6 +9,8 @@ from .forms import PostForm
 from .models import Post
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit = False)
@@ -55,6 +57,8 @@ def post_list(request):
  
 
 def post_update(request,id = None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post,id = id)
 	form = PostForm(request.POST or None, request.FILES or None, instance= instance)
 	if form.is_valid():
@@ -70,6 +74,8 @@ def post_update(request,id = None):
 	return render(request,"post_form.html", context)
 
 def post_delete(request,id=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post,id = id)
 	instance.delete()
 	messages.success(request, "successfully removed !!!")
